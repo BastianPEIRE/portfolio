8f4eb0a2fd7d5a5da70b76b097dcb5022312d081
bstnp <54482913+BastianPEIRE@users.noreply.github.com>
2023-12-26 16:02:56 +0000

    Update 10 files

---
+++ app/components/project/project.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function project() {
+  return (
+    <Link href="">
+      <div>
+        <h2>{}</h2>
+        <p>{}</p>
+      </div>
+    </Link>
+  );
+}
